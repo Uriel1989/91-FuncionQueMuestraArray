3e08d3451f75437f49e0d742862bdd9f7ead3de9
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:58:21 -0300

    Add files via upload

---
+++ 91-FuncionQueMuestraArray/Array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _91_FuncionQueMuestraArray
+{
+    class Array
+    {
+        public void mostrarArray(int[] array)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+        }
+    }
+}
---
+++ 91-FuncionQueMuestraArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//Crea una función que muestre un array.
+
+namespace _91_FuncionQueMuestraArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Array contenido = new Array();
+
+            contenido.mostrarArray(valores);
+        }
+    }
+}
